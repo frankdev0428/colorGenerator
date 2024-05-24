cd275136e44c9d3f72ec88859d324cf0d9e0b7cc
frankdev0428@gmail.com <frankdev0428@gmail.com>
2024-05-24 10:21:08 -0700

    implement stylesheet

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-const colors  = ['#b0726a','#b06a7c'];
+const colors = ['#ae6a87', '#af637e', '#bc7673', '#be7986', '#af6972', '#b06282', '#be7976', '#bf6a70', '#b17574', '#ab6c86', '#a5746d', '#b37587', '#b8627c', '#a8757d', '#a8647a', '#b76883', '#b16d7d', '#a7657c', '#a5676d', '#ad6f6e'];
+
+const button = document.querySelector('.button');
+const color = document.querySelector('.color');
+
+button.addEventListener('click', () => {
+    const randomNumber  = getRandomNumber();
+    document.body.style.backgroundColor = colors[randomNumber];
+    console.log(randomNumber);
+    color.textContent = colors[randomNumber]
+})
+
+function getRandomNumber() {
+  const colorLength = colors.length;
+    return Math.floor(Math.random()*colorLength);
+}
