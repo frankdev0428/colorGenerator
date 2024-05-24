efd6159456c12f2cbb34e5c969cb09938d65f588
frankdev0428@gmail.com <frankdev0428@gmail.com>
2024-05-24 13:48:11 -0700

    Finalize Color Generetor app

--- colorPicker/app.js
+++ colorPicker/app.js
@@ -1,4 +1,4 @@
-const colors = ['#ae6a87', '#af637e', '#bc7673', '#be7986', '#af6972', '#b06282', '#be7976', '#bf6a70', '#b17574', '#ab6c86', '#a5746d', '#b37587', '#b8627c', '#a8757d', '#a8647a', '#b76883', '#b16d7d', '#a7657c', '#a5676d', '#ad6f6e'];
+const colors = ['Red', 'Blue', 'Yellow', 'Orange', 'Black', 'Grey', 'Green', 'Aqua', 'Purple', '#ab6c86', '#a5746d', '#b37587', '#b8627c', '#a8757d', '#a8647a', '#b76883', '#b16d7d', '#a7657c', '#a5676d', '#ad6f6e'];
 
 const button = document.querySelector('.button');
 const color = document.querySelector('.color');
