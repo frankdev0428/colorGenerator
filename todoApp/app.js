ab26750175bfb53c599f539940285968db8f3d51
frankdev0428@gmail.com <frankdev0428@gmail.com>
2024-07-25 22:52:02 -0700

    taskData consumer implement

--- todoApp/app.js
+++ todoApp/app.js
@@ -31,5 +31,11 @@ taskForm.addEventListener('submit', (e) => {
   const dataArrIndex = taskData.findIndex((item) =>item.id === currentTask.id );
   const taskObj = {
     id: `${titleInput.value.toLowerCase().split(" ").join('-')}-${Date.now()}`,
+    title: titleInput.value,
+    date: dateInput.value,
+    description: descriptionInput.value,
   }
+  if ( dataArrIndex === -1) {
+    taskData.unshift(taskObj)
+  }  
 })
