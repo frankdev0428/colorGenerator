62686d2c4a444902eabe980b235dcfb0f410eb3e
frankdev0428@gmail.com <frankdev0428@gmail.com>
2024-07-27 11:21:26 -0700

    Generate taskForm

--- todoApp/app.js
+++ todoApp/app.js
@@ -7,35 +7,53 @@ const cancelBtn = document.getElementById("cancel-btn");
 const discardBtn = document.getElementById("discard-btn");
 const tasksContainer = document.getElementById("tasks-container");
 const titleInput = document.getElementById("title-input");
+const dateInput = document.getElementById("date-input");
+const descriptionInput = document.getElementById("description-input");
+
 const taskData = [];
 let currentTask = {};
 
-openTaskFormBtn.addEventListener("click", () => {
-  taskForm.classList.toggle("hidden");
-});
+openTaskFormBtn.addEventListener("click", () =>
+  taskForm.classList.toggle("hidden")
+);
+
 closeTaskFormBtn.addEventListener("click", () => {
   confirmCloseDialog.showModal();
 });
+
 cancelBtn.addEventListener("click", () => confirmCloseDialog.close());
+
 discardBtn.addEventListener("click", () => {
   confirmCloseDialog.close();
   taskForm.classList.toggle("hidden");
 });
 
-// Use const to declare a variable called dataArrIndex and assign it the value of taskData.findIndex(). For the findIndex() method, pass in an arrow function with item as the parameter.
-
-// Within the arrow function, check if the id property of item is strictly equal to the id property of currentTask.
-
-taskForm.addEventListener('submit', (e) => {
+taskForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  const dataArrIndex = taskData.findIndex((item) =>item.id === currentTask.id );
+
+  const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
   const taskObj = {
-    id: `${titleInput.value.toLowerCase().split(" ").join('-')}-${Date.now()}`,
+    id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
     title: titleInput.value,
     date: dateInput.value,
     description: descriptionInput.value,
+  };
+
+  if (dataArrIndex === -1) {
+    taskData.unshift(taskObj);
   }
-  if ( dataArrIndex === -1) {
-    taskData.unshift(taskObj)
-  }  
-})
+
+  taskData.forEach(({ id, title, date, description }) => {
+    tasksContainer.innerHTML += `
+          <div class="task" id="${id}">
+            <p><strong>Title:</strong> ${title}</p>
+            <p><strong>Date:</strong> ${date}</p>
+            <p><strong>Description:</strong> ${description}</p>
+            <button type="button" class="btn">Edit</button>   
+            <button type="button" class="btn">Delete</button> 
+            
+          </div>
+        `;
+  });
+   taskForm.classList.toggle('hidden');
+});
