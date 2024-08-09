4bf88e8a3646907253669269347cd56e6dca4ced
frankdev0428@gmail.com <frankdev0428@gmail.com>
2024-08-08 18:55:25 -0700

    Implement input field value

---
+++ BinaryConverter/app.js
@@ -0,0 +1,7 @@
+const numberInput = document.getElementById('number-input');
+const result = document.getElementById('result');
+const convertBtn = document.getElementById('convert-btn');
+
+function checkUserInput() {
+  
+}
