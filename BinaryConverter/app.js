6562028fb0a1697f14884f308934c4e63af40712
frankdev0428@gmail.com <frankdev0428@gmail.com>
2024-08-18 21:49:51 -0700

    function update

--- BinaryConverter/app.js
+++ BinaryConverter/app.js
@@ -1,7 +1,36 @@
-const numberInput = document.getElementById('number-input');
-const result = document.getElementById('result');
-const convertBtn = document.getElementById('convert-btn');
+const numberInput = document.getElementById("number-input");
+const convertBtn = document.getElementById("convert-btn");
+const result = document.getElementById("result");
 
-function checkUserInput() {
-  
-}
+const decimalToBinary = (input) => {
+  const inputs = [];
+  const quotients = [];
+  const remainders = [];
+  while(input > 0){
+    const quotient = Math.floor(input/2);
+    input = quotient;
+    
+  }
+};
+
+const checkUserInput = () => {
+  if (
+    !numberInput.value ||
+    isNaN(parseInt(numberInput.value)) ||
+    parseInt(numberInput.value) < 0
+  ) {
+    alert("Please provide a decimal number greater than or equal to 0");
+    return;
+  }
+
+  decimalToBinary(parseInt(numberInput.value));
+  numberInput.value = "";
+};
+
+convertBtn.addEventListener("click", checkUserInput);
+
+numberInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    checkUserInput();
+  }
+});
