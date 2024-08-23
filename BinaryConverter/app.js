d8f12be39638f146c29248b2a3be34a476a0f033
frankdev0428@gmail.com <frankdev0428@gmail.com>
2024-08-23 12:45:46 -0700

    Function update

--- BinaryConverter/app.js
+++ BinaryConverter/app.js
@@ -2,17 +2,16 @@ const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
+
 const decimalToBinary = (input) => {
-  let binary = "";
-
-  while (input > 0) {
-
-    input = Math.floor(input / 2);
-    
-    
+  if(input === 0) {
+    return '0'
+  } else if (input === 1){
+    return '1'
+  } else {
+    return decimalToBinary(Math.floor(input/2)) + (input % 2);
   }
 
-  result.innerText = binary;
 };
 
 const checkUserInput = () => {
@@ -26,6 +25,7 @@ const checkUserInput = () => {
   }
 
   decimalToBinary(parseInt(numberInput.value));
+  result.textContent = decimalToBinary();
   numberInput.value = "";
 };
 
