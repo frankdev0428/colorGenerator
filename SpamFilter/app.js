d5243a215837294fc97a9600a4c15fce7307c4c6
frankdev0428@gmail.com <frankdev0428@gmail.com>
2024-09-07 23:06:03 -0700

    Implement match case for msg parameter

---
+++ SpamFilter/app.js
@@ -0,0 +1,15 @@
+const messageInput = document.getElementById("message-input");
+const result = document.getElementById("result");
+const checkMessageButton = document.getElementById("check-message-btn");
+const helpRegex = /pleas help/i;
+
+const isSpam = (msg) => msg.match(helpRegex); 
+
+checkMessageButton.addEventListener("click", () => {
+   if(messageInput.value === ''){
+     alert("Please enter a message.");
+     return;
+   }
+   result.textContent = isSpam(messageInput.value) ? "Oh no! this looks like a spam message." : "This message does not seem to contain any spam."
+   messageInput.value = '';  
+});
