96fa901b41eddb0a978d450aac441852aea57157
frankdev0428@gmail.com <frankdev0428@gmail.com>
2024-09-18 22:22:23 -0700

    Create updateUI array

--- numsSorter/sort.js
+++ numsSorter/sort.js
@@ -2,7 +2,9 @@ const sortButton = document.getElementById('sort');
 
 const sortInputArray = (event) => {
     event.preventDefault();
-   const inputValues = document.getElementsByClassName('values-dropdown');
+   const inputValues = [...document.getElementsByClassName('values-dropdown')].map((dropdown) => Number(dropdown.value));
    
+
 };
+const updateUI = (array = []) => {};
 sortButton.addEventListener('click',sortInputArray);
