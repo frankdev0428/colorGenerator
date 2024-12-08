284b7f57a9e580c41b9a4debe83ca8f8bc48cae1
frankdev0428@gmail.com <frankdev0428@gmail.com>
2024-12-08 09:49:56 -0800

    Setting up shpping cart project

--- SpreadSheet/main.js
+++ SpreadSheet/main.js
@@ -29,6 +29,8 @@ const spreadsheetFunctions = {
     sum,
     average,
     median,
+    even : (nums) => nums.filter(isEven),
+    
 }
 const applyFunction = (str) => {
     const noHigh = highPrecedence(str);
@@ -37,7 +39,8 @@ const applyFunction = (str) => {
     const functionCall = /([a-z0-9]*)\(([0-9., ]*)\)(?!.*\()/i;
     const toNumberList = (args) => args.split(',').map(parseFloat);
     const apply = (fn,args) => spreadsheetFunctions[fn.toLowerCase()](toNumberList(args));
-    
+    return str2.replace(functionCall,(match,fn,args) => spreadsheetFunctions.hasOwnProperty(fn.toLowerCase()) ? apply(fn,args) : match );
+  
 }
 
 
@@ -52,7 +55,8 @@ const evalFormula = (x,cells) => {
     const rangeExpanded = x.replace(rangeRegex,(_match,char1,num1,char2,num2) => rangeFromString(num1,num2).map(addCharacters(char1))(char2));
     const cellRegex = /[A-Z][1-9][0-9]?/gi;
     const cellExpanded  = rangeExpanded.replace(cellRegex,(match) => idToText(match.toUpperCase()));
-    
+    const functionExpanded = applyFunction(cellExpanded);
+    return functionExpanded === x ? functionExpanded : evalFormula(functionExpanded, cells)
 
 }
 window.onload = () => {
@@ -82,7 +86,8 @@ const update = (event) => {
    const element = event.target;
    const value = element.value.replace(/\s/g,'');
    if(!value.includes(element.id) && value[0] === "="){
-    
+     element.value = evalFormula(value.slice(1),Array.from(document.getElementById('container').children));
+  
    }
 
 }
