23d34e00241ed0cdb464cdd981fe71769f3cc6d9
frankdev0428@gmail.com <frankdev0428@gmail.com>
2024-12-09 14:16:02 -0800

    implement Shopping cart counting features

--- ShoppingCart/main.js
+++ ShoppingCart/main.js
@@ -81,4 +81,33 @@ const products = [
     price: 12.99,
     category: "Cupcake",
   },
-];
+  
+  products.forEach(
+    ({ name, id, price, category }) => {
+      dessertCards.innerHTML += `
+         <div class="dessert-card">
+          <h2>${name}</h2>
+          <p class="dessert-price">$${price}</p>
+          <p class="product-category">Category: ${category}</p>
+          <button id="${id}" class="btn add-to-cart-btn">Add to cart</button>
+
+         </div>
+      `;
+    }
+  );
+
+  class ShoppingCart {
+    constructor() {
+      this.items = [];
+      this.total = 0;
+      this.taxRate = 8.25;
+    }
+    addItem(id,products) {
+      const product = products.find((item) => item.id === id);
+      const {name,price} = product;
+      this.items.push(product);
+      const totalCountPerProduct = {};
+      this.items.forEach((dessert) => {})
+    }
+
+  } ;
